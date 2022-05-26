108510e4a831c0d65249d07f01e7bdfad76f00f7
Andrei Aquino <andreimartinbaquino@gmail.com>
2022-05-27 01:46:23 +0800

    added age calculator method

--- src/com/company/forpractice/FindDuplicates.java
+++ src/com/company/forpractice/FindDuplicates.java
@@ -1,8 +1,12 @@
 package com.company.forpractice;
 
+import java.time.LocalDate;
+import java.time.Period;
+
 public class FindDuplicates {
     public static void main(String[] args) {
-        checker("How many duplicates are there?");
+//        checker("How many duplicates are there?");
+        ageCalc(1987, 12, 9);
     }
 
     public static void checker(String sentence) {
@@ -23,5 +27,16 @@ public class FindDuplicates {
         }
         System.out.println("DUPLICATES: " + duplicates);
         System.out.println("CHARACTERS: " + characters);
+    }
+
+    public static void ageCalc(int year, int mo, int day ) {
+        LocalDate today = LocalDate.now();
+        LocalDate bday = LocalDate.of(year, mo, day);
+
+        System.out.println("Today: " + today + " Birthday: " + bday);
+        int numOfDays = Period.between(bday, today).getDays();
+        int numOfMonths = Period.between(bday, today).getMonths();
+        int numOfYears = Period.between(bday, today).getYears();
+        System.out.println("Years old: " + numOfYears + " , # of months: " + numOfMonths + " , # of days: " + numOfDays);
     }
 }
